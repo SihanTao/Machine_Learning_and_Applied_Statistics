50241564aac6cd7ed355c056cdebf644f07763c0
Sihan Tao <kevintao0626@gmail.com>
2022-07-11 22:39:17 +0100

    ProblemClass1

--- Week2/ProblemClass1/problemClass1.R
+++ Week2/ProblemClass1/problemClass1.R
@@ -1,31 +1,27 @@
 # Read in the data:
-MyData <- read.csv("EUR-GBP-Rates.csv", header = TRUE, sep = ",",
-                     dec = ".",fileEncoding="UTF-8-BOM")
+
+MyData \<- read.csv("EUR-GBP-Rates.csv", header = TRUE, sep = ",", dec = ".",fileEncoding="UTF-8-BOM")
 
 # View the data
+
 View(MyData)
 
 # Determine the number of observations in MyData
+
 nrow(MyData)
 
 str(MyData)
 
 # print all the values of the GBPtoEUR
-MyData$GBPtoEUR
-MyData[,3]
 
-plot(MyData$GBPtoEUR, type = 'l', col = 'blue', main = "EUR/GBP exchange rate",
-     xlab = "Time", ylab = "GBP per EUR", cex.main = 1.25, cex.lab = 1.5, 
-     cex.axis = 2)
+MyData\$GBPtoEUR MyData[,3]
+
+plot(MyData\$GBPtoEUR, type = 'l', col = 'blue', main = "EUR/GBP exchange rate", xlab = "Time", ylab = "GBP per EUR", cex.main = 1.25, cex.lab = 1.5, cex.axis = 2)
 
 # Transfer Data
-MyDates = as.Date(MyData$Date, "%d/%m/%Y")
-plot(MyDate, MyData$GBPtoEUR, type = 'l')
+
+MyDates = as.Date(MyData$Date, "%d/%m/%Y") plot(MyDate, MyData$GBPtoEUR, type = 'l')
 
 # Now we can compare the initial plot with the modified plot
-par(mfrow=c(1, 2))
-plot(MyData$GBPtoEUR)
-plot(MyDates, MyData$GBPtoEUR, type="l",col="blue", main="EUR/GBP exchange rate", xlab="Time", ylab="GBP per one EUR", cex.main
-     =1.25, cex.lab=1.5, cex.axis=2)
-par(mfrow=c(1,1))
 
+par(mfrow=c(1, 2)) plot(MyData$GBPtoEUR) plot(MyDates, MyData$GBPtoEUR, type="l",col="blue", main="EUR/GBP exchange rate", xlab="Time", ylab="GBP per one EUR", cex.main =1.25, cex.lab=1.5, cex.axis=2) par(mfrow=c(1,1))
